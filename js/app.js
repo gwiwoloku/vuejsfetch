fe90c68a3ef042c93719e3ff8e5e126d793c0a2b
gwiwoloku <gwiwoloku@gmail.com>
2021-02-18 12:56:05 +0000

    #comment added tabs

--- js/app.js
+++ js/app.js
@@ -4,28 +4,49 @@ const vm = new Vue({
   el: '#app',
   data: {
     results: [],
+    currencies:[],
     countDown: 5,
+    lastUpdated : null,
+    activeItem: 'home'
   },
-  mounted() {
-
-    axios.get(url).then(response => {
-      this.results = response.data
-    }).then(() => {
-      setInterval(() => {
-        if(this.countDown === 0) {
-          this.countDown = 5
-          axios.get(url).then(response => {
-            this.results = response.data
-          })
-        } else if(this.countDown > 0)this.countDown -= 1
-      }, 1000)
-    });
+  created () {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.getExchangeData();
+    setInterval(() => {
+      if(this.countDown === 0) {
+        this.countDown = 5
+        this.getExchangeData()
+      } else if(this.countDown > 0)this.countDown -= 1
+    }, 1000)
   },
-  method: {
-    getData: function () {
-      return axios.get(url).then(response => {
-        this.results = response.data
-      })
+  methods: {
+    getNow: function() {
+      const today = new Date();
+      const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      this.lastUpdated = date +' '+ time;
+    },
+    getExchangeData() {
+      axios.get(url).then(response => {
+        if(!response.ok){
+          throw new Error('Generic error')
+        }
+        return response.data;
+      }).then(response => {
+        this.getNow();
+        this.results = response;
+        // Adding all charCodes to currency array
+        for (let currency in this.results) {
+          this.currencies.push(currency)
+        }
+      });
+    },
+    isActive (menuItem) {
+      return this.activeItem === menuItem
+    },
+    setActive (menuItem) {
+      this.activeItem = menuItem
     }
-  },
+  }
 });
